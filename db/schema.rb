e51e6622500d8be379eb10424e40ec289aee4da6
yuki-kobashi <kd1344505@st.kobedenshi.ac.jp>
2024-11-12 13:19:03 +0900

    messeagesテーブルの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_22_042757) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_12_041607) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "messages", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
